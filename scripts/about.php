96a3ec4876cf8bbf9c433c34274b79f86fd7a806
Rugema <58324049+Rugema01@users.noreply.github.com>
2022-03-17 00:49:29 +1000

    Add files via upload

---
+++ scripts/about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Survey | About</title>
+    <link rel="stylesheet" href="../styles/about.css">
+</head>
+
+<body>
+    <header>
+        <div class="menu">
+            <a href="../index.html"><img src="../media/logo.png" title="Online Survey"></a>
+            <a href="../index.html">
+                <h1>Online Survey</h1>
+            </a>
+            <nav id="navs">
+                <a href="../index.html">Home</a>
+                <a href="survey.php">Survey</a>
+                <a href="help.php">Help</a>
+            </nav>
+        </div>
+    </header>
+    <article>
+        <center>
+            <div class="intro">
+                <h1><b><u>Introduction</u></b></h1>
+                <p>This site have made for know and correct some different information from different peoples.</p>
+                <a href="">Know More</a>
+                <h1><b><u>Our Aims</u></b></h1>
+                <p>We protect clients informations and trying make solutions about review problems.</p>
+                <a href="">Read More</a>
+                <h1><b><u>Future Plans</u></b></h1>
+                <p>Our future goals kindly consider to the above issues that may against our every day lifes.</p>
+                <a href="">Learn More</a>
+            </div>
+        </center>
+    </article>
+    <footer><center>
+    <div class="social">
+            <a href="https://www.facebook.com/zebronin" id="fb">Facebook - </a>
+            <a href="https://www.instagram.com/zebron.in" id="ista">Instagram - </a>
+            <a href="https://twitter.com/ZebronIn" id="twi">Twitter</a>
+        </div>
+        <div class="end">
+            <small>Zebron In. &copy; 2022 | All rights reserved</small>
+        </div>
+    </footer>
+    </footer>
+</body>
+
+</html>
---
+++ scripts/help.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Survey | Help</title>
+    <link rel="stylesheet" href="../styles/help.css">
+</head>
+
+<body>
+    <header>
+        <div class="menu">
+            <a href="../index.html"><img src="../media/logo.png" title="Online Survey"></a>
+            <a href="../index.html">
+                <h1>Online Survey</h1>
+            </a>
+            <nav id="navs">
+                <a href="../index.html">Home</a>
+                <a href="survey.php">Survey</a>
+                <a href="about.php">About</a>
+            </nav>
+        </div>
+    </header>
+    <article>
+        <center>
+            <div class="intro">
+                <h1><b><u>How to survey</u></b></h1>
+                <p>To complete the survey you must answer all asked questions freely because we offer to you no time limites and you can also gain online survey certificate.</p>
+                <a href="survey.php">Get Started</a>
+            </div>
+        </center>
+    </article>
+    <footer><center>
+    <div class="social">
+            <a href="https://www.facebook.com/zebronin" id="fb">Facebook - </a>
+            <a href="https://www.instagram.com/zebron.in" id="ista">Instagram - </a>
+            <a href="https://twitter.com/ZebronIn" id="twi">Twitter</a>
+        </div>
+        <div class="end">
+            <small>Zebron In. &copy; 2022 | All rights reserved</small>
+        </div>
+    </footer>
+    </footer>
+</body>
+
+</html>
